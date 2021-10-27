7920e1ab90bde0dc3048ec60291af8abe6d44bb8
rose830115 <rose830164@gmail.com>
2021-10-28 00:26:31 +0800

    Modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 const app = express()
 const port = 3000
 const db = mongoose.connection
@@ -19,7 +20,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()//取出 Todo model 裡的所有資料
+    .lean()//把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos }))// 將資料傳給 index 樣板
+    .catch(error => console.error(error))// 錯誤處理
+  
 })
 
 app.listen(port, () => {
