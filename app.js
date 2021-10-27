294fde6055a0e89bdc73672c45dcaad8c377de74
rose830115 <rose830164@gmail.com>
2021-10-27 17:48:50 +0800

    Connect to mongodb database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+const db = mongoose.connection
+
+mongoose.connect('mongodb://localhost/todo-list')
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world')
