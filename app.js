20ff3f574f9ca3f3bbf2409e70954ff9a2559bff
rose830115 <rose830164@gmail.com>
2021-10-28 01:34:45 +0800

    Add create todo function

--- app.js
+++ app.js
@@ -18,13 +18,27 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
+//進入首頁，抓取資料並渲染
 app.get('/', (req, res) => {
   Todo.find()//取出 Todo model 裡的所有資料
     .lean()//把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos }))// 將資料傳給 index 樣板
-    .catch(error => console.error(error))// 錯誤處理
-  
+    .catch(error => console.error(error))// 錯誤處理  
+})
+
+//進入新增頁面
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+//新增todo
+app.post('/todos', (req, res) => {
+  const name = req.body.name//接住表單資料
+  Todo.create({ name })// 存入資料庫
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
