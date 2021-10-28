43fda0a2a1fb640834bb0087c610a5c36d57c9fb
rose830115 <rose830164@gmail.com>
2021-10-28 15:44:39 +0800

    Add update todo function

--- app.js
+++ app.js
@@ -20,35 +20,59 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
-//進入首頁，抓取資料並渲染
+//R進入首頁，抓取資料並渲染
 app.get('/', (req, res) => {
-  Todo.find()//取出 Todo model 裡的所有資料
+  return Todo.find()//取出 Todo model 裡的所有資料
     .lean()//把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos }))// 將資料傳給 index 樣板
     .catch(error => console.error(error))// 錯誤處理  
 })
 
-//進入新增頁面
+//R進入新增頁面
 app.get('/todos/new', (req, res) => {
   res.render('new')
 })
 
-//新增todo
+//C新增todo
 app.post('/todos', (req, res) => {
   const name = req.body.name//接住表單資料
-  Todo.create({ name })// 存入資料庫
+  return Todo.create({ name })// 存入資料庫
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
-//進入detail頁面
+//R進入detail頁面
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
-  Todo.findById(id)
+  return Todo.findById(id)
     .lean()
     .then((todo) => res.render('datail', { todo }))
     .catch(error => console.error(error))
 })
+
+
+//R進入修改頁面
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.error(error))
+})
+
+//U修改todo
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
 
 app.listen(port, () => {
   console.log(`app is running on localhost:${port}`)
