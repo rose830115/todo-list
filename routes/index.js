77e98ae586cee3f01bafb38b3787ae4d21855e20
rose830115 <rose830164@gmail.com>
2022-01-04 12:14:33 +0800

    Add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
