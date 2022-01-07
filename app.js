20ffd4411885f0561eabf9742819332b496120f7
rose830115 <rose830164@gmail.com>
2022-01-07 17:15:44 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
